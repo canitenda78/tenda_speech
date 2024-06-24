c3bc14308f78f0e7798bd3c1c7951ceea39bd8eb
canitenda78 <canitenda78@gmail.com>
2024-06-24 22:12:40 +0900

    ログイン機能追加

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+    public function showLoginForm(Request $request)
+    {
+        return view('auth.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController; 
 use App\Http\Controllers\TemplateController; 
 use App\Http\Controllers\SiteController; 
 
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 //post/createにアクセスした際にPostControllerのcreate()を実行する
 Route::get('post/create', [PostController::class, 'create'])->name('post.create');
@@ -28,3 +40,7 @@ Route::get('/new-master', [TemplateController::class, 'index']);
 
 // //site/topを表示するメソッドへのルート
 Route::get('/site/top', [SiteController::class, 'index'])->name('site.top');
+
+//ログイン画面に遷移するルート
+Route::get('/login', [LoginController::class, 'showLoginForm']);
+
